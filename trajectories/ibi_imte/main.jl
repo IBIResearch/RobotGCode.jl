20a5b4232f6240af394b3c9782703e1d36d7ac96
Artyom Tsanda <artyom.tsanda@tuhh.de>
2026-04-30 16:57:05 +0200

    ibi imte measurement

--- trajectories/ibi_imte/main.jl
+++ trajectories/ibi_imte/main.jl
@@ -1,25 +1,39 @@
 using RobotGCode
 
-ibi = with_z(string_curve("  IBI"), 0.04)
-imte = with_z(translated(string_curve("IMTE"), (0.0, -1.0)), 0.08)
-curve = merged(ibi, imte)
+X_MIN, X_MAX = -150., 150.
+Y_MIN, Y_MAX = -170., 170.
+Z = -35.0
 
-npoints = 200
-points = discretize(curve; npoints=npoints)
+START = (-160.93, 200.0, 0.0)
+SHALTER = (-160.93, 249.61, 0.0)
 
-max_coordinates = maximum(points, dims=1)
-min_coordinates = minimum(points, dims=1)
+curve = merged(
+    string_curve("  IBI"),
+    translated(string_curve("IMTE"), (0.0, -1.0))
+)
+curve = fit_to_box(curve, (X_MIN, X_MAX), (Y_MIN, Y_MAX))
+curve = with_z(curve, Z)
 
-center = (max_coordinates .+ min_coordinates) ./ 2
-center[3] = 0
-range2d = maximum((max_coordinates .- min_coordinates)[1:2]) / 2
 
-points .-= center
-points[:, 1] ./= range2d
-points[:, 2] ./= range2d
+curve_start = point_at(curve, 0.0)
+curve_end = point_at(curve, 1.0)
 
-points[:, 1] .*= 0.18
-points[:, 2] .*= 0.18
+shalter_to_curve = line_segment_3d(SHALTER, curve_start)
+curve_to_shalter = line_segment_3d(curve_end, SHALTER)
 
-fig = visualize_positions_3d(points; xlims=(-0.18, 0.18), ylims=(-0.18, 0.18), zlims=(0.0, 0.13))
+curve = merged(shalter_to_curve, curve)
+curve = merged(curve, curve_to_shalter)
+
+points = discretize(curve; npoints=500)
+
+fig = visualize_positions_3d(points)
 display(fig)
+
+generate_gcode(
+    points[2:end, :] ./ 1000;
+    ausgabe_datei = joinpath("/home/tsanda/nextcloud/Research Projects/MMR/Code/GCode Roboter/General_Trajectory/Files", "ibi_imte.gcode"),
+    frame_time = 1.0,
+    offset = (0.0, 0.0, 0.0),
+    start = START,
+    shalter = SHALTER,
+)
