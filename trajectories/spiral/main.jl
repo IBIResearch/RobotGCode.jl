cfa145116333e07606425c09ac5463984bce12ee
Artyom Tsanda <artyom.tsanda@tuhh.de>
2026-05-04 09:43:28 +0200

    spiral curve

--- trajectories/spiral/main.jl
+++ trajectories/spiral/main.jl
@@ -3,6 +3,13 @@ using RobotGCode
 DATA_FOLDER = "./data/spiral"
 mkpath(DATA_FOLDER)
 
+X_MIN, X_MAX = -160., 160.
+Y_MIN, Y_MAX = -170., 170.0
+Z_MIN, Z_MAX = -94.0, -30.0
+
+START = (-160.93, 200.0, 0.0)
+SHALTER = (-160.93, 249.61, 0.0)
+
 """
 	spiral(t; turns=6)
 
@@ -21,22 +28,31 @@ function spiral(t; turns::Real=6, r_margin=0.1)
 	y = radius * sin(theta)
 	z = t_clamped
 
-	return (x, y, z)
+	return (x, y, z*0.3)
 end
 
-npoints = 400
-points = discretize(spiral; npoints=npoints)
 
-points[:, 1] .*= 0.18
-points[:, 2] .*= 0.18
-points[:, 3] .*= 0.13
+curve = fit_to_box(spiral, (X_MIN, X_MAX), (Y_MIN, Y_MAX), (Z_MIN, Z_MAX))
 
-fig = visualize_positions_3d(points; xlims=(-0.18, 0.18), ylims=(-0.18, 0.18), zlims=(0.0, 0.13))
+curve_start = point_at(curve, 0.0)
+curve_end = point_at(curve, 1.0)
+
+shalter_to_curve = line_segment_3d(SHALTER, curve_start)
+curve_to_shalter = line_segment_3d(curve_end, SHALTER)
+
+curve = merged(shalter_to_curve, curve)
+curve = merged(curve, curve_to_shalter)
+
+points = discretize(curve; npoints=500)
+
+fig = visualize_positions_3d(points)
 display(fig)
 
 generate_gcode(
-	points;
-	ausgabe_datei = joinpath(DATA_FOLDER, "spiral.gcode"),
-	frame_time = 1.0,
-	offset = (0.0, 0.0, 0.0),
+    points[2:end, :] ./ 1000;
+    ausgabe_datei = joinpath("/home/tsanda/nextcloud/Research Projects/MMR/Code/GCode Roboter/General_Trajectory/Files", "spiral.gcode"),
+    frame_time = 1.0,
+    offset = (0.0, 0.0, 0.0),
+    start = START,
+    shalter = SHALTER,
 )
